7bd80635f99acd0bf3b828c7bda20198e65a2b5a
Aanyabhadouria23 <aanyabhadouria23@gmail.com>
2024-11-26 22:08:47 +0530

    Add files via upload
    C ++ programming

---
+++ first.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+int main()
+
+    std::count<<''hello world'';
+         return 0;
+}
+
+
+int main()
+
+    std::count<<"my name is aanya bhadouria ">>
+          return0;
+
+
+
+
+using name std;
+
+int main()
+{ 
+ int age; (const char [15] "how old are you ?"
+count<<"how old are you ?" << endl ;
+cin >> age;
+count<< "ok,you are " << age << "year(s) old " <<endl;
+    
+      return0;
+
+ using name std;
+
+ int main()
+
+ int num1 , num2 , num3;
+ cunt <<" enter the first number " ;
+ cin >> num1;
+ count<< " enter the second number ";
+ cin>> num2;
+
+ num3=num1+num2;
+ count<< "sum of number " <<"_ " << num3 ";
+     
+
+     return0;
+
+
+
+
+using name std;
+
+int main()
+
+class swap;
+
+{  
+int n1,n2,temp;
+public;
+void take ()
+
+        count << " enter two number ";
+        cin>>n1>>n2
+
+        return0;
+
+
+
+
+
+
+}
+
+
+using name std;
+int main()
+{  
+count<<"size of float" << sizeof(float)<<"bytes"<< endl;
+count<<"size of int " << sizeof(int)<<"bytes"<< endl;
+count<<"size of char " << sizeof(char)<<"byte"<<endl;
+count<<" size of double" << sizeof(double)"bytes"<<endl;
+system(paused);
+}
+
