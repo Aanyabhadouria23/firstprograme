8c335218b3d409b0b2d94aa3926889215852c036
Aanyabhadouria23 <aanyabhadouria23@gmail.com>
2024-11-26 21:23:30 +0530

    Add files via upload
    cpp file

---
+++ for c.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main (main) {
+      
+printf("hello world \n");
+
+
+exit(0);
+
+      int a;
+      int b;
+
+      printf("enter the first number:")
+      scanf("%d" , &);
+
+      printf("enter the second number :") ;
+      scanf("%d" , &b );
+
+      int c=a+b ; 
+      printf("the sum of two number is : %d , c) ;
+      
+      
+
+      getch();
+      clsr();
+
+}
